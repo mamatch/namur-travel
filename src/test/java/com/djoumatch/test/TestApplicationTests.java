c290cfe81e68d3bd39b776887646fbb900f3f553
Djoimatch <dej@Djoimatchs-MacBook-Pro.local>
2023-03-04 18:28:38 +0100

    init

---
+++ src/test/java/com/djoumatch/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djoumatch.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
